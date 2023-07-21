b6f09902c73ea518efa1fd32b3836c0866dac36b
Liu Xiang <lxmtlab@gmail.com>
2023-07-21 18:02:57 +0800

    添加下网站地址

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '哒鸽', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '吾不生产知识，只做知识的搬运工😁', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://dage110.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'NLP, 阅读笔记, 随笔', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
